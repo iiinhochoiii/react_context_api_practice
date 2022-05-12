a7a80818d5f89ad8bc6ed79788d80d2a5e3b04c1
dlsgh120 <dlsgh120@gmail.com>
2022-05-12 22:21:30 +0900

    eslint prettier 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        trailingComma: 'es5',
+        singleQuote: true,
+        semi: true,
+        printWidth: 80,
+      },
+    ],
+  },
+};
